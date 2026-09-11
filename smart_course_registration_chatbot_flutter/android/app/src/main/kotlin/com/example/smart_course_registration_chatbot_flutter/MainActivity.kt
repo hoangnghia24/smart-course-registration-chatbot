db4334865a1bcd580b6fe42efbc3ac9cdd2e4711
hoangnghia24 <dqhn2403@gmail.com>
2026-09-11 23:56:14 +0700

    chore: initialize smart course registration chatbot

---
+++ smart_course_registration_chatbot_flutter/android/app/src/main/kotlin/com/example/smart_course_registration_chatbot_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_course_registration_chatbot_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ smart_course_registration_chatbot_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:smart_course_registration_chatbot_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
